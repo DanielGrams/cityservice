1116fa162c263c4ff2ef68065c36ddf5418367af
Daniel Grams <grams.daniel@gmail.com>
2020-03-20 13:56:09 +0100

    scraper

--- scrape.py
+++ scrape.py
@@ -1,10 +1,90 @@
 from app import app, db
 from models import NewsItem
+import feedparser
+from pprint import pprint
+import datetime
+from dateutil import parser, tz
+import pytz
 
 def scrape():
-    item = NewsItem(content='Hello', publisher_name='Katermann')
+    now = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+    min_date = now - datetime.timedelta(days=7)
 
-    db.session.add(item)
+    scrape_feed(
+        now,
+        min_date,
+        'https://www.goslar.de/presse/pressemitteilungen?format=feed&type=rss',
+        'Stadt Goslar',
+        'https://www.goslar.de/templates/ndk_bs3_j3x/tpls/designs/ndk-goslar/ico/favicon-192x192.png')
+    scrape_feed(
+        now,
+        min_date,
+        'https://www.landkreis-goslar.de/media/rss/Pressemitteilung.xml',
+        'Landkreis Goslar',
+        'https://www.landkreis-goslar.de/media/logo-start.gif')
+    scrape_feed(
+        now,
+        min_date,
+        'https://www.kwb-goslar.de/media/rss/Pressemitteilungen.xml',
+        'KWB Goslar',
+        'https://www.kwb-goslar.de/layout/kwb/media/logo_kwb-goslar.png')
+    scrape_feed(
+        now,
+        min_date,
+        'http://www.presseportal.de/rss/dienststelle_56518.rss2',
+        'Polizei Goslar',
+        'https://www.polizei-nds.de/favicon.ico')
+    scrape_feed(
+        now,
+        min_date,
+        'https://stadtbibliothek.goslar.de/stadtbibliothek/aktuelles?format=feed&type=rss',
+        'Stadtbibliothek Goslar',
+        'https://www.goslar.de/templates/ndk_bs3_j3x/tpls/designs/ndk-goslar/ico/favicon-192x192.png')
+
+    delete_old_items(min_date)
+
+def scrape_feed(now, min_date, url, publisher_name, publisher_icon_url):
+    try:
+        print(url)
+        channel = feedparser.parse(url)
+
+        for entry in channel.entries:
+
+            if ('published' not in entry or
+                'id' not in entry or
+                'title' not in entry or
+                'link' not in entry):
+                continue
+
+            published = parser.parse(entry.published)
+            if published < min_date:
+                continue
+
+            item = NewsItem.query.filter_by(source_id = entry.id).first()
+            item_did_exist = False
+            if item is None:
+                item = NewsItem(source_id = entry.id)
+                db.session.add(item)
+            else:
+                item_did_exist = True
+
+            item.publisher_name = publisher_name
+            item.publisher_icon_url = publisher_icon_url
+            item.content = entry.title
+            item.link_url = entry.link
+            item.published = published
+            item.fetched = now
+
+            if not item_did_exist:
+                db.session.add(item)
+
+    except Exception as e:
+        pprint(e)
+    finally:
+        db.session.commit()
+
+def delete_old_items(min_date):
+    NewsItem.query.filter(NewsItem.published <= min_date).delete()
     db.session.commit()
 
 if __name__ == '__main__':
