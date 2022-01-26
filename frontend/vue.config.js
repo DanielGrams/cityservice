fa920d7c761744959eaf4d9f22e6c5d0965b83d7
Daniel Grams <grams.daniel@gmail.com>
2022-01-26 08:47:00 +0100

    vue-i18n

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-    outputDir: '../project/static/frontend',
-    devServer: {
-      proxy: {
-        '/api*': {
-          target: 'http://localhost:5000/'
-        }
+  outputDir: '../project/static/frontend',
+
+  devServer: {
+    proxy: {
+      '/api*': {
+        target: 'http://localhost:5000/'
       }
     }
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'de',
+      fallbackLocale: 'de',
+      localeDir: 'locales',
+      enableInSFC: true,
+      enableBridge: false
+    }
   }
+}
