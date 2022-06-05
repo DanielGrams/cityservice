a036b1608b90e499f304775c3db41db83b60bb58
Daniel Grams <grams.daniel@gmail.com>
2022-06-05 17:23:28 +0200

    NavBar

--- frontend/src/app/places/views/List.vue
+++ frontend/src/app/places/views/List.vue
@@ -12,56 +12,5 @@
 </template>
 
 <script>
-import httpService from "@/services/http.service";
-export default {
-  data() {
-    return {
-      errorMessage: null,
-      fields: [
-        {
-          key: "name",
-        },
-      ],
-      totalRows: 0,
-      currentPage: 1,
-      perPage: 10,
-      searchResult: {
-        items: [],
-      },
-    };
-  },
-  methods: {
-    loadTableData(ctx, callback) {
-      const vm = this;
-      httpService
-        .get(`/api/places`, {
-          params: {
-            page: ctx.currentPage,
-            per_page: ctx.perPage,
-          },
-          handleRequestStart: () => (this.errorMessage = null),
-          handleRequestError: /* istanbul ignore next */ (
-            _error,
-            message
-          ) => /* istanbul ignore next */ {
-            this.errorMessage = message;
-            callback([]);
-          },
-        })
-        .then((response) => {
-          vm.totalRows = response.data.total;
-          callback(response.data.items);
-        });
-      return null;
-    },
-    onRowSelected(items) {
-      /* istanbul ignore next */
-      if (items.length < 1) {
-        return;
-      }
-
-      this.$router.push({ path: `places/${items[0].id}` });
-    },
-  },
-};
+export default {};
 </script>
